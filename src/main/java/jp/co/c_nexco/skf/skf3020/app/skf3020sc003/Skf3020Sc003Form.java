d41fe15bf09d9478b513975c34a645ac94b42778
ya.kawasaki <kawasaki837@oki.com>
2019-07-31 17:15:58 +0900

    転任者情報取込、確認画面（ひな形）追加

---
+++ src/main/java/jp/co/c_nexco/skf/skf3020/app/skf3020sc003/Skf3020Sc003Form.java
@@ -0,0 +1,16 @@
+package jp.co.c_nexco.skf.skf3020.app.skf3020sc003;
+
+import jp.co.c_nexco.nfw.webcore.app.BaseForm;
+
+import java.util.*;
+
+import lombok.EqualsAndHashCode;
+
+@lombok.Data
+@EqualsAndHashCode(callSuper = true)
+public class Skf3020Sc003Form extends BaseForm {
+	
+	private static final long serialVersionUID = 5285848663756806913L;
+
+
+}
