1087a1f18dae352a50219c0668067512d9eb23eb
k.hashimoto <k.hashimoto@srsys.jp>
2019-11-11 19:47:04 +0900

    法定調書データ管理　追加

--- src/main/java/jp/co/c_nexco/skf/skf3070/app/skf3070sc003/Skf3070Sc003Form.java
+++ src/main/java/jp/co/c_nexco/skf/skf3070/app/skf3070sc003/Skf3070Sc003Form.java
@@ -1,6 +1,21 @@
+/*
+ * Copyright(c) 2020 NEXCO Systems company limited All rights reserved.
+ */
 package jp.co.c_nexco.skf.skf3070.app.skf3070sc003;
 
+import jp.co.c_nexco.skf.skf3070.app.skf3070common.Skf3070CommonForm;
+import lombok.EqualsAndHashCode;
 
-public class Skf3070Sc003Form {
+/**
+ * 賃貸人（代理人）所有物件一覧画面同期処理Form Skf3070Sc003Form
+ * 
+ * @author NEXCOシステムズ
+ *
+ */
+@lombok.Data
+@EqualsAndHashCode(callSuper = true)
+public class Skf3070Sc003Form extends Skf3070CommonForm {
+
+	private static final long serialVersionUID = 5285848663756806913L;
 
 }
